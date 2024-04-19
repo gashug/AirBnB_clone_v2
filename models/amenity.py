6622dfb31cbed66bc3022b0a69f5795ae4fd2ec4
gashug <geoffrey.gachuiri98@gmail.com>
2024-04-19 06:59:46 -0700

    Updated some tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python
-""" holds class Amenity"""
+""" Module that holds class Amenity """
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    """ Representation of Amenity """
+    __tablename__ = 'amenities'
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    name = Column(String(128), nullable=False)
+
+    if models.storage_t == 'db':
+        place_amenities = relationship(
+            "Place", secondary="place_amenity", back_populates="amenities")
